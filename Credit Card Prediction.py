408a49d86fdca40dd4cf73c397ca5dfb5fe3ffbe
YasiChen <yasi.chen@duke.edu>
2020-07-31 13:44:55 -0400

    Initial commit

---
+++ Credit Card Prediction.py
@@ -0,0 +1,8 @@
+# this is a kaggle project for predicting the credit card approval/credit card default
+# in this project, i will first understand the data, and then do data preparation
+# modeling will utilize the machine learning methods
+# finally i will talk about the evaluation of the models
+
+
+# intsall data in from my local env
+
